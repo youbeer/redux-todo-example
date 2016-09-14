5a39a1c19a9d2655cbc440dd95be95ff521a2e61
you <youbeer23@gmail.com>
2016-09-14 14:40:41 +0900

    added textbox

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
+    <AddTodo />
     <VisibleTodoList />
   </div>
 )
